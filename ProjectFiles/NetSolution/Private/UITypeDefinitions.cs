7570b02672152db1b1b9207c5a99f7e739490fb1
craig1850 <craigsd@outlook.com>
2026-04-03 18:26:08 -0500

    Initial commit

---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,101 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "b331a1c78292b402b9fb7bea8d50a6ad")]
+public class HomeScreen : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "ba060dabe3223c9b73ecd35e8a6579f1")]
+public class DateAndTimeWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "a023c02f13e5746f6e661e46fb207e49")]
+public class ColorPickerDialog : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "93cbf19dd68a72c543a41f03c3c2b7a1")]
+public class NetworkSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "343e9a553a1648e3de95b91f6d877b43")]
+public class DeviceSettingsWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "41382c57dc616862c1af276ff6942abc")]
+public class FTRemoteAccessWidget : FTOptix.UI.Screen
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "41f433c486aa6dfdb14c2c47570eea1a")]
+public class LAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "323ecbd80fe0dccc66307959b261f54a")]
+public class EmptyPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "d17d8ab3d7f8047c35a02cbe0b7248c6")]
+public class WAN : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "632709bc0ca708c922da2f68cb20eb09")]
+public class Diagnostics : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "9cf154c923572624320f16c16fa657a3")]
+public class Configuration : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "548a589cc1532b072d9d24316f7b6090")]
+public class Product_Information : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "e348b14dbc7035688714c7ced8a72128")]
+public class ConnectionPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "c05a81edcb91fa074772f19543ea1b38")]
+public class AcceptDenyIncomingRequestPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "d8a8bb0d6c0490587ebdf9c1eb42375e")]
+public class NewConnectionCredentialsPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "77122192dcccfaa39d1e78d7bd27e222")]
+public class RequestAssistancePanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "f0afd51b3f49febd40b32153974e81b8")]
+public class RequestAssistanceDisabledPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ColorPickerImproved", Guid = "dc2c41b238ed9ce9aa4f98d28d04b528")]
+public class RequestAssistanceEnabledPanel : FTOptix.UI.Panel
+{
+}
